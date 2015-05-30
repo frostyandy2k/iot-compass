4558778e0586e3d8ec7dace745a5795da900e9c9
Andrei Miclaus <miclaus@teco.edu>
2015-05-30 22:05:54 +0200

    added a website for tills office

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Kitchen' });
 });
 
+router.get('/TillsOffice', function(req, res) {
+  res.render('tillsoffice', { title: 'Tills Office' });
+});
+
 router.get('/preview', function(req, res) {
   res.render('index', { title: 'Kitchen' });
 });
