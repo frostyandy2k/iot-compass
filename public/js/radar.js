8a6e37c5eef0b8d0407eba6a1e5178f825156d68
Jack Unseld <jackunseld@gmail.com>
2015-05-31 23:35:48 +0200

    demo v1,removed buttons,directions in text,added ajax (get data of different rooms)

--- public/js/radar.js
+++ public/js/radar.js
@@ -64,32 +64,45 @@ function initRadar(divSelector, items) {
     .style("stroke", "#ff6f00")
     .attr("fill","none");
 
-  generatePattern(svg, 50, "img/flower.jpg", 'flowerpattern');
-  generatePattern(svg, 100, "img/flower.jpg", 'flowerpatternFull');
+  // generatePattern(svg, 50, "img/flower.jpg", 'flowerpattern');
+  // generatePattern(svg, 100, "img/flower.jpg", 'flowerpatternFull');
 
-  generatePattern(svg, 50, "img/microwave.png", 'microwavepattern');
-  generatePattern(svg, 100, "img/microwave.png", 'microwavepatternFull');
+  // generatePattern(svg, 50, "img/microwave.png", 'microwavepattern');
+  // generatePattern(svg, 100, "img/microwave.png", 'microwavepatternFull');
 
-  generatePattern(svg, 50, "img/lamp.png", 'lamppattern');
-  generatePattern(svg, 100, "img/lamp.png", 'lamppatternFull');
+  // generatePattern(svg, 50, "img/lamp.png", 'lamppattern');
+  // generatePattern(svg, 100, "img/lamp.png", 'lamppatternFull');
 
-  generatePattern(svg, 50, "img/coffeemachine.jpg", 'coffeemachinepattern');
-  generatePattern(svg, 100, "img/coffeemachine.jpg", 'coffeemachinepatternFull');
+  // generatePattern(svg, 50, "img/coffeemachine.jpg", 'coffeemachinepattern');
+  // generatePattern(svg, 100, "img/coffeemachine.jpg", 'coffeemachinepatternFull');
 
-  generatePattern(svg, 50, "img/fridge.jpg", 'fridgepattern');
-  generatePattern(svg, 100, "img/fridge.jpg", 'fridgepatternFull');
-
+  // generatePattern(svg, 50, "img/fridge.jpg", 'fridgepattern');
+  // generatePattern(svg, 100, "img/fridge.jpg", 'fridgepatternFull');
 
   $.each(items, function(key, val){
+    if(val.img) {
+      generatePattern(svg, 50, val.img, key+'pattern');
+      generatePattern(svg, 100, val.img, key+'patternFull');
+    } else {
+      var img = "img/" + key + ".png"
+      generatePattern(svg, 50, img, key+'pattern');
+      generatePattern(svg, 100, img, key+'patternFull');
+    }
     var x = -radarradius*Math.sin((val.location.dir)*Math.PI/180);
     var y = -radarradius*Math.cos((val.location.dir)*Math.PI/180);
     // console.log(x,y)
-    svg.append("circle")
-      .attr("class", "items " + val.uri)
+      // .append("svg:a")
+      // .attr("class", "page-scroll")
+      // .attr("xlink:href", "#"+key)
+    svg
+      .append("circle")
+      .attr("onclick", "$('#"+key+"')[0].scrollIntoView()")
+      .attr("cursor", "pointer")
+      .attr("id", key+"radar")
       .attr("r", itemradius)
       .attr("transform", "translate("+x+"," + y + ")")
       .style("stroke", "black")
-      .attr("fill","url(#"+val.uri+"pattern)");
+      .attr("fill","url(#"+key+"pattern)");
   });
 }
 var showItems = false;
@@ -108,17 +121,17 @@ function updatePositions(items, direction) {
     // $('#radartarget1').html(Math.round(getLocation().dir) + " " + Math.round(actualDirection));
     var x = -radarradius*Math.sin(actualDirection*Math.PI/180);
     var y = -radarradius*Math.cos(actualDirection*Math.PI/180);
-    d3.select("."+val.uri)
+    d3.select("#"+key+"radar")
       .attr("transform", "translate("+x+", "+y+")");
   });
 }
 
 function showItem(uri) {
   $.each(items, function(key, value) {
-    if(uri == 'all' || uri == value.uri) {
-      document.getElementById(value.uri).style.display = 'block';
+    if(uri == 'all' || uri == key) {
+      document.getElementById(key).style.display = 'block';
     } else {
-      document.getElementById(value.uri).style.display = 'none';
+      document.getElementById(key).style.display = 'none';
     }
   })
 }
@@ -165,26 +178,29 @@ function initListeners() {
   })
   document.addEventListener('foundItemInFront', function(item) {
     item = item.detail;
-    $('#radartarget').html(item.uri);
+    $('#radartarget').html(item.key);
 
-    $('#selectionCircle').attr("fill","url(#"+item.uri+"patternFull)");
+    $('#selectionCircle').attr("fill","url(#"+item.key+"patternFull)");
     $('#mini_radar_icon').hide();
     $('#mini_radar_selection').show();
-    $('#mini_radar_selection').attr('src', 'img/' + item.uri + '.png');
+    $('#mini_radar_selection').attr('src', 'img/' + item.key + '.png');
  
-    if(!vibrating || (valURIold != item.uri)){
-      valURIold = item.uri;
+    if(!vibrating || (valURIold != item.key)){
+      valURIold = item.key;
       vibrating = true;
       navigator.vibrate(100);
     }
   })
 }
 function getIndianaData() {
-  indiana.getData(function(items) {
+  indiana.getRoomData(function(items) {
     console.log("getData -> Items", items)
     $("#radardiv").show();
     initRadar('#radardiv', items);
     initListeners();
+    setInterval(function() {
+      applyLocationTextChildren(items);
+    }, 500)
   });
 }
 function resetIndianaOrientation() {
@@ -194,11 +210,101 @@ function hasGetUserMedia() {
   return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
             navigator.mozGetUserMedia || navigator.msGetUserMedia);
 }
-$(document).ready(function() {
-    indiana = spatialAwareness();
-    $("#radardiv").hide();
-    $("#qrcodediv").hide();
+// data-locationaware
+// das ist nur eine beispielanwendung
+function applyLocationTextChildren(registeredThings){
+  $.each(registeredThings, function(key, value) {
+    var location = indiana.getThingCardinalPosition(key);
+    var id = '#' + key;
+    console.log(location)
+    // if (!$(id+"Position")) $(id).append('<p id="'+id+'Position"></p>');
+    switch(location) {
+      case 'N': createLocationText(id, key, 'in front'); break;
+      case 'NW': createLocationText(id, key, 'in front and to the left'); break;
+      case 'NE': createLocationText(id, key, 'in front and to the right'); break;
+      case 'SW': createLocationText(id, key, 'behind to the left'); break;
+      case 'EW': createLocationText(id, key, 'behind to the right'); break;
+      case 'S': createLocationText(id, key, 'behind', true); break;
+      case 'E': createLocationText(id, key, 'right'); break;
+      case 'W': createLocationText(id, key, 'left'); break;
+    }
+  })
+}
 
+function createLocationText(id, thing, location, no_of){
+    var of = no_of ? '' : ' of';
+    $(id+"Position").html("<strong>The "+thing+" is "+location+of+" you.</strong>");
+}
+
+$(document).ready(function() {
+  $("#radardiv").hide();
+  $("#qrcodediv").hide();
+  indiana = spatialAwareness();
+  getIndianaData();
+  // initDeviceMotion();
     // if(hasGetUserMedia()) readqrcode();
     // else console.log("Browser doesn't support video capture.")
 });
+function initDeviceMotion() {
+
+  // Position Variables
+  var x = 0;
+  var y = 0;
+  var z = 0;
+  // Speed - Velocity
+  var vx = 0;
+  var vy = 0;
+  var vz = 0;
+   
+  // Acceleration
+  var ax = 0;
+  var ay = 0;
+  var az = 0;
+   
+  var delay = 10;
+  var vMultiplier = delay/1000;
+  window.ondevicemotion = function(data) {
+    // $("#qrcodedivdata").html(Math.round(Orientation.detail.dir));
+    // console.log("deviceorientation", data)
+    var accel2 = data.accelerationIncludingGravity;
+    var accel = data.acceleration;
+    // console.log('accel gravity:',accel)
+    // console.log('accel:',accel2)
+    ax = Math.round(accel.x*precision)/precision;
+    ay = Math.round(accel.y*precision)/precision;
+    az = Math.round(accel.z*precision)/precision;
+  }
+  
+  var epsilon = 0.0001;
+  var precision_digits = 6;
+  var precision = 10^(precision_digits);
+  setInterval(function() {
+    $('#qrcodedivdata').html('accel x: ' + ax.toFixed(precision_digits) 
+      + '\ny: ' + ay.toFixed(precision_digits) + '\nz: ' + az.toFixed(precision_digits));
+    vy = vy + ay*vMultiplier;
+    vx = vx + ax*vMultiplier;
+    vz = vz + az*vMultiplier;
+ 
+
+    // var ball = document.getElementById("ball");
+    // if(Math.abs(ay) > epsilon) 
+    y = (y + vx*vMultiplier + ay/2 * vMultiplier*vMultiplier);
+    y = Math.round(y*precision)/precision;
+    // if(Math.abs(ax) > epsilon) 
+    x = (x + vy*vMultiplier + ax/2 * vMultiplier*vMultiplier);
+    x = Math.round(x*precision)/precision;
+    // if(Math.abs(az) > epsilon) 
+    z = (z + vz*vMultiplier + az/2 * vMultiplier*vMultiplier);
+    z = Math.round(z*precision)/precision;
+    $('#qrcodediverr').html('pos x: ' + x.toFixed(precision_digits) 
+      + '\ny: ' + y.toFixed(precision_digits) + '\nz: ' + z.toFixed(precision_digits));
+    // if (x<0) { x = 0; vx = 0; }
+    // if (y<0) { y = 0; vy = 0; }
+    // if (x>document.documentElement.clientWidth-20) { x = document.documentElement.clientWidth-20; vx = 0; }
+    // if (y>document.documentElement.clientHeight-20) { y = document.documentElement.clientHeight-20; vy = 0; }
+    
+    // ball.style.top = y + "px";
+    // ball.style.left = x + "px";
+    // document.getElementById("pos").innerHTML = "x=" + x + "<br />y=" + y;
+  }, delay);
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Kitchen' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/kitchen', function(req, res) {
+  res.render('kitchen', { title: 'Kitchen' });
 });
 
 router.get('/tillsoffice', function(req, res) {
@@ -14,8 +18,60 @@ router.get('/matthiasoffice', function(req, res) {
   res.render('matthiasoffice', { title: 'Matthias Office' });
 });
 
-router.get('/preview', function(req, res) {
-  res.render('index', { title: 'Kitchen' });
-});
 
+router.get('/kitchen/items', function(req, res) {
+	var kitchen_items = {
+	  "microwave": {
+	    location: {dir: 350},
+	  },
+	  "flower": {
+	    location: {dir: 270},
+	    img: "img/flower.jpg"
+	  },
+	  "lamp": {
+	    location: {dir: 80},
+	    controlON: "http://cumulus.teco.edu:81/21345gjphtnch87/ON",
+	    controlOFF: "http://cumulus.teco.edu:81/21345gjphtnch87/OFF"
+	  },
+	  "coffeemachine": {
+	    location: {dir: 190},
+	    img: "img/coffeemachine.jpg"
+	  },
+	  "fridge": {
+	    location: {dir: 200},
+	    img: "img/fridge.jpg"
+	  }
+	};
+  res.json(kitchen_items);
+});
+router.get('/tillsoffice/items', function(req, res) {
+	var tillsoffice_items = {
+	  "thesis": {
+	    location: {dir: 80},
+	  },
+	  "flower": {
+	    location: {dir: 300},
+	    img: "img/flower.jpg"
+	  },
+	  "telephone": {
+	    location: {dir: 160},
+	  }
+	};
+  res.json(tillsoffice_items);
+});
+router.get('/matthiasoffice/items', function(req, res) {
+	var matthiasoffice_items = {
+	  "cardboard": {
+	    location: {dir: 190},
+	  },
+	  "flower": {
+	    location: {dir: 250},
+	    img: "img/flower.jpg"
+	  },
+	  "budde": {
+	    location: {dir: 0},
+	  }
+	};
+  res.json(matthiasoffice_items);
+});
 module.exports = router;
